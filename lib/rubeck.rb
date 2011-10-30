c1638af815c06e625c60422a4a5bf2886bb1e72b
Artem Titoulenko <artem.titoulenko@gmail.com>
2011-10-30 19:27:08 -0400

    roots. Rubeck test works. It's now awesome.

--- lib/rubeck.rb
+++ lib/rubeck.rb
@@ -1,5 +1,4 @@
 require 'json'
-require 'zmq'
 
 require './lib/rubeck/connection'
 require './lib/rubeck/fiber_pool'
--- lib/rubeck/connection.rb
+++ lib/rubeck/connection.rb
@@ -10,35 +10,40 @@ class Connection
     @sub_addr = zmq_sub_pub_addr[0..1].join(":")
     @pub_addr = zmq_sub_pub_addr[2..3].join(":")
     
+    puts "initialized with sub, pub addr: " + @sub_addr + ', ' + @pub_addr
+    
     @request_sock = @response_sock = nil
   end
   
   def connect
     context = ZMQ::Context.new 1
-    @request_sock = context.socket ZMQ::UPSTREAM
+    @request_sock = context.socket ZMQ::PULL
     @request_sock.connect @sub_addr
     
     @response_sock = context.socket ZMQ::PUB
     @response_sock.setsockopt ZMQ::IDENTITY, @app_id
     @response_sock.connect @pub_addr
+    
+    puts "made req_sock, res_sock: " + @request_sock.inspect + ", " + @response_sock.inspect
   end
   
   #raw recv
   def recv
-    data = @request_sock.recv
-    return data
+    puts "'recv'ing.."
+    return @request_sock.recv(0)
   end
   
   #parse the request, this is the best way to get stuff back, as a Hash
   def receive
-    data = self.recv
-    return parse(data)
+    return parse(self.recv)
   end
   
   def send(uuid, conn_id, msg)
     header = "%s %d:%s" % [uuid, conn_id.length, conn_id.join(' ')]
     string =  header + ', ' + msg 
+    puts "'send'ing string: " + string
     @response_sock.send(string, 0)
+    return
   end
   
   def reply(request, message)
