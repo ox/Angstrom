2b48e760196075be589d79b19de70ca8dc3df03d
Artem Titoulenko <artem.titoulenko@gmail.com>
2011-10-31 11:22:28 -0400

    cleaning

---
+++ demo/armstrong_test.rb
@@ -0,0 +1,10 @@
+require '../lib/armstrong'
+
+class HelloHandler < WebMessageHandler
+  def process(req)
+    output("Hello world")
+  end
+end
+
+app = Armstrong.new [[/\//, HelloHandler]]
+app.run!
